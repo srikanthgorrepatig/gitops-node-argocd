64db9c5f0ee7e52424893784a3cf8a2c7a2dd824
srikanthgorrepatig <srikanthawsos@gmail.com>
2025-11-14 12:09:29 +0530

    test: verifying CI/CD pipeline

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
 res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.end('Hello from Node.js on K8s!\n');
+res.end('Hi from Node.js on K8s!\n');
 });
 server.listen(port, () => console.log(`Server listening on ${port}`));
